fbd370b8e29554d9988e633d2908bec33129e88d
rustam-1999 <tolliboyevrustam1999@gmail.com>
2022-11-03 21:01:37 +0500

    initol commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var elBody=document.querySelector('body')
 var elbutton=document.querySelector('.button')
+// var elbutton=document.querySelector('.body-button')
+
 elbutton.addEventListener("click",function(){
     elBody.classList.toggle('dark');
 })
